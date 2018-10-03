fe7ca5c0f1cc714e7e58aec66d3783bfac6991f7
IvanPJF <teaching-light@yandex.ru>
2018-10-03 22:14:07 +0300

    002.6.3. if-else-throw [#49895]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -7,41 +7,40 @@ package ru.job4j.tracker;
  *@version 0.1
  */
 public class StubInput implements Input {
-	private String[] answers;
-	private int position;
+    private String[] answers;
+    private int position;
 
-	public StubInput(String[] answers) {
-		this.answers = answers;
-	}
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
 
-	/**
-	 * Задаём вопрос, получаем ответ.
-	 * @param question Вопрос пользователю.
-	 * @return Эмуляция ответа от пользователя.
-	 */
-	public String ask(String question) {
-		return this.answers[position++];
-	}
+    /**
+     * Задаём вопрос, получаем ответ.
+     * @param question Вопрос пользователю.
+     * @return Эмуляция ответа от пользователя.
+     */
+    public String ask(String question) {
+        return this.answers[position++];
+    }
 
-	/**
-	 * Задаём вопрос, получаем ответ.
-	 * @param question Вопрос пользователю.
-	 * @param range Набор возможных вариантов выбора меню.
-	 * @return Эмуляция ответа от пользователя.
-	 */
-	public int ask(String question, int[] range) {
-		int key = Integer.valueOf(this.ask(question));
-		boolean exist = false;
-		for (int index: range) {
-			if (index == key) {
-				exist = true;
-				break;
-			}
-		}
-		if (exist) {
-			return key;
-		} else {
-			throw new MenuOutException("Out of menu range.");
-		}
-	}
+    /**
+     * Задаём вопрос, получаем ответ.
+     * @param question Вопрос пользователю.
+     * @param range Набор возможных вариантов выбора меню.
+     * @return Эмуляция ответа от пользователя.
+     */
+    public int ask(String question, int[] range) {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int index: range) {
+            if (index == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            throw new MenuOutException("Out of menu range.");
+        }
+        return key;
+    }
 }
